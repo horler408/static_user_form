9562769bda81feffcfb80fbbed5b32c7ae22aa16
horler408 <horler.web@gmail.com>
2020-10-31 23:25:31 +0100

    form validation continues

--- public/js/app.js
+++ public/js/app.js
@@ -5,25 +5,25 @@ const password = document.getElementById('pswd');
 const confirmPassword = document.getElementById('confirm-pswd')
 const registerForm = document.querySelector('.register_form')
 
-// registerForm.addEventListener('submit', () => {
-//     if(firstName.value == '' && lastName.value == '' && email.value == '' && password.value == '') {
-//         alert('All necessary field must be filled')
-//         return false
-//     }
+registerForm.addEventListener('submit', () => {
+    if(firstName.value == '' && lastName.value == '' && email.value == '' && password.value == '') {
+        alert('All necessary field must be filled')
+        return false
+    }
 
-//     if(password.length < 8) {
-//         alert('password must be atlest 8 characters long')
-//         return false
-//     }
+    if(password.length < 8) {
+        alert('password must be atlest 8 characters long')
+        return false
+    }
 
-//     if(password.value === confirmPassword.value) {
-//         alert('Password must match')
-//         return false
-//     }
-//     else {
-//         return true
-//     }
-// })
+    if(password.value === confirmPassword.value) {
+        alert('Password must match')
+        return false
+    }
+    else {
+        return true
+    }
+})
 
 function validateForm() {
     if(firstName.value == '' && lastName.value == '' && email.value == '' && password.value == '') {
